49dc452157a475e8386534459ec54a757b316316
RUTUJ GOPAL JAJU <f20251259@pilani.bits-pilani.ac.in>
2026-08-27 22:26:02 +0000

    Fix Task 3 files and test dataflow CLA

--- labs/lab01/task3/rca.v
+++ labs/lab01/task3/rca.v
@@ -1,49 +1,30 @@
-// rca.v
-
-// Delayed 4-bit ripple-carry adder using four FA_Gate instances.
-
 module rca(
-
   input  [3:0] a,
   input  [3:0] b,
   input        cin,
   output [3:0] sum,
   output       cout
-
 );
-
   wire c1, c2, c3;
 
-  FA_Gate FA0 (
-    .a(a[0]),
-    .b(b[0]),
-    .cin(cin),
-    .sum(sum[0]),
-    .cout(c1)
-  );
+  FA_Gate FA0 (.a(a[0]), .b(b[0]), .cin(cin), .sum(sum[0]), .cout(c1));
+  FA_Gate FA1 (.a(a[1]), .b(b[1]), .cin(c1),  .sum(sum[1]), .cout(c2));
+  FA_Gate FA2 (.a(a[2]), .b(b[2]), .cin(c2),  .sum(sum[2]), .cout(c3));
+  FA_Gate FA3 (.a(a[3]), .b(b[3]), .cin(c3),  .sum(sum[3]), .cout(cout));
+endmodule
 
-  FA_Gate FA1 (
-    .a(a[1]),
-    .b(b[1]),
-    .cin(c1),
-    .sum(sum[1]),
-    .cout(c2)
-  );
-
-  FA_Gate FA2 (
-    .a(a[2]),
-    .b(b[2]),
-    .cin(c2),
-    .sum(sum[2]),
-    .cout(c3)
-  );
-
-  FA_Gate FA3 (
-    .a(a[3]),
-    .b(b[3]),
-    .cin(c3),
-    .sum(sum[3]),
-    .cout(cout)
-  );
-
+// Defined here so the compiler finds it without needing a new file
+module FA_Gate(
+  input  a,
+  input  b,
+  input  cin,
+  output sum,
+  output cout
+);
+  wire ps, pc1, pc2;
+  xor (ps,  a,   b);
+  and (pc1, a,   b);
+  xor (sum, cin, ps);
+  and (pc2, cin, ps);
+  or  (cout, pc1, pc2);
 endmodule
